98ab1e904b23cad9699a220932e3185b495053fb
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-05-17 18:06:05 +0700

    menambahkan int main

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -32,3 +32,22 @@ mahasiswa::mahasiswa(int iNim, string iNama) {
 	nama = iNama;
 }
 
+void mahasiswa::cetak() {
+	cout << endl << "Nim =" << nim << endl;
+	cout << "Nama = " << nama << endl;
+
+}
+
+int main() {
+	mahasiswa mhs1;
+	mahasiswa mhs2(20);
+	mahasiswa mhs3("Indra");
+	mahasiswa mhs4(20, "Fauzan");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
+	return 0;
+}
